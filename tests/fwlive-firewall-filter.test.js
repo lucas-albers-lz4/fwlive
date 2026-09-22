89d5c2ab6386a136ea5410bc76557a11e32c1ad4
Lucas Albers <lucas.b.albers@gmail.com>
2026-09-21 21:24:18 -0600

    test(parser): include underscore deny fixture

--- tests/fwlive-firewall-filter.test.js
+++ tests/fwlive-firewall-filter.test.js
@@ -64,10 +64,10 @@ function run() {
 	const iptFixture = path.join(__dirname, 'fixtures', 'logread-iptables.json');
 	const iptPayload = JSON.parse(fs.readFileSync(iptFixture, 'utf8'));
 	const iptStats = core.statsLogEntries(iptPayload.log);
-	assert.equal(iptStats.firewall, 7);
+	assert.equal(iptStats.firewall, 8);
 	assert.equal(iptStats.noise, 1);
 	const iptRows = core.filterLogEntries(iptPayload.log);
-	assert.equal(iptRows.length, 7);
+	assert.equal(iptRows.length, 8);
 	assert.ok(iptRows.some((r) => r.rule_hint === 'fwlive-ping'));
 	assert.ok(iptRows.some((r) => r.action === 'drop' && r.interface_in === 'wan'));
 
